6d79928375163a06d335927deaffe620fb3be223
abhi-cyber <abhirajchatrath@gmail.com>
2022-04-26 20:18:46 +0530

    Flex | Center

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    container: { // By default will set all the containers in the center
-      center: true,
-      padding: '2rem',
-    }
+    // container: { // By default will set all the containers in the center
+    //   center: true,
+    //   padding: '2rem',
+    // }
   },
   plugins: [],
   // The pruge command removes unused/ unwanted css from the public/style.css
